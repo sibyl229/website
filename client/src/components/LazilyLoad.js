5758a6cf28fb9f3c7a42d7e4d7d49f125a9247b2
Sibyl <sibyl@wormbase.org>
2017-02-28 16:13:08 +0000

    remove React from this PR

--- client/src/components/LazilyLoad.js
+++ client/src/components/LazilyLoad.js
@@ -1,65 +1,3 @@
-import React from 'react';
-
-class LazilyLoad extends React.Component {
-
-  constructor() {
-    super(...arguments);
-    this.state = {
-      isLoaded: false,
-    };
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-    this.load();
-  }
-
-  componentDidUpdate(previous) {
-    if (this.props.modules === previous.modules) return null;
-    this.load();
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  load() {
-    this.setState({
-      isLoaded: false,
-    });
-
-    const { modules } = this.props;
-    const keys = Object.keys(modules);
-
-    Promise.all(keys.map((key) => modules[key]()))
-      .then((values) => (keys.reduce((agg, key, index) => {
-        agg[key] = values[index];
-        return agg;
-      }, {})))
-      .then((result) => {
-        if (!this._isMounted) return null;
-        this.setState({ modules: result, isLoaded: true });
-      });
-  }
-
-  render() {
-    if (!this.state.isLoaded) return null;
-    return React.Children.only(this.props.children(this.state.modules));
-  }
-}
-
-LazilyLoad.propTypes = {
-  children: React.PropTypes.func.isRequired,
-};
-
-export const LazilyLoadFactory = (Component, modules) => {
-  return (props) => (
-    <LazilyLoad modules={modules}>
-      {(mods) => <Component {...mods} {...props} />}
-    </LazilyLoad>
-  );
-};
-
 export const importLazy = (promise, {globalName}={}) => (
   promise.then((result) => {
     if (result && result.default) {
@@ -69,5 +7,3 @@ export const importLazy = (promise, {globalName}={}) => (
     }
   })
 );
-
-export default LazilyLoad;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import './index.css';
 import '../legacy_modules/css/jquery-ui.min.css';
 import './main.css';
 import './wormbase.js';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
