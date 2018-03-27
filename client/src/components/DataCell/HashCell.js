9da9a7d4ebce8cf90fee17b6570bd006ea5328ae
Sibyl <sibyl@wormbase.org>
2018-03-27 17:10:03 +0000

    wip style HashCell and ListCell

--- client/src/components/DataCell/HashCell.js
+++ client/src/components/DataCell/HashCell.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
 import { hasContent } from './utils';
 //import SimpleCell from './SimpleCell';
 
 const HashCell = (props) => {
-  const {data, render} = props;
+  const {data, render, classes} = props;
   return (
-    <ul>
+    <div>
     {
       Object.keys(data).filter((key) => (
         hasContent(data[key])
       )).map((key) => (
-        <li key={key}>
-        {key.replace(/_+/g, ' ')}:
-        <br/>
-        {render({elementValue: data[key]})}
-        </li>
+        <div key={key} className={classes.pair}>
+          <div className={classes.key}>{key.replace(/_+/g, ' ')}: </div>
+          <div className={classes.value}>{render({elementValue: data[key]})}</div>
+        </div>
       ))
     }
-    </ul>
+    </div>
   );
 };
 
 HashCell.propTypes = {
   data: PropTypes.object,
-  render: PropTypes.func.isRequired
+  render: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
 };
 
-export default HashCell;
+const styles = (theme) => {
+  return {
+    pair: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      margin: theme.spacing.unit / 2,
+      padding: 0,
+    },
+    key: {
+      fontWeight: 700,
+      flex: '0 1 auto',
+    },
+    value: {
+    },
+  }
+};
+
+export default withStyles(styles, { withTheme: true })(HashCell);
--- client/src/components/DataCell/ListCell.js
+++ client/src/components/DataCell/ListCell.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
 import { hasContent } from './utils';
 
+
 const ListCell = (props) => {
-  const {data, render} = props;
+  const {data, render, classes} = props;
   return (
-    <ul>
+    <ul className={classes.ul}>
     {
       data.filter((dat) => (
         hasContent(dat)
       )).map((dat, index) => (
-        <li key={index}>{render({elementData: dat})}</li>
+        <li key={index} className={classes.li}>
+          {render({elementData: dat})}
+        </li>
       ))
     }
     </ul>
@@ -19,7 +23,23 @@ const ListCell = (props) => {
 
 ListCell.propTypes = {
   data: PropTypes.arrayOf(PropTypes.any),
-  render: PropTypes.func
+  render: PropTypes.func,
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+
 };
 
-export default ListCell;
+const style = (theme) => {
+  return {
+    ul: {
+      margin: 0,
+      padding: `0 ${theme.spacing.unit}px`,
+      listStyleType: 'none',
+    },
+    li: {
+
+    },
+  };
+};
+
+export default withStyles(style, {withTheme: true})(ListCell);
