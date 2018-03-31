33db0760892d97ecb3b6fd57144f0a1ea956e64d
Sibyl <sibyl@wormbase.org>
2018-03-31 02:36:30 +0000

    fix ie11 issue by loading polyfill

--- client/src/index.js
+++ client/src/index.js
@@ -13,22 +13,10 @@ function browserSupportsAllFeatures() {
   return window.Promise && window.fetch && window.Symbol;
 }
 
-function loadScript(src, done) {
-  var js = document.createElement('script');
-  js.src = src;
-  js.onload = function() {
-    done();
-  };
-  js.onerror = function() {
-    done(new Error('Failed to load script ' + src));
-  };
-  document.head.appendChild(js);
-}
-
 if (browserSupportsAllFeatures()) {
   // Browsers that support all features run `main()` immediately.
   main();
 } else {
   // All other browsers loads polyfills and then run `main()`.
-  loadScript('https://cdn.polyfill.io/v2/polyfill.min.js?features=default-3.6,fetch', main);
+  import('babel-polyfill').then(() => main());
 }
