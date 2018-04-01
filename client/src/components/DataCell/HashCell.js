dce00aa9e348a74fddf34130847f45d9cea1870d
Sibyl <sibyl@wormbase.org>
2018-04-01 16:32:53 +0000

    fix style of hash cell key

--- client/src/components/DataCell/HashCell.js
+++ client/src/components/DataCell/HashCell.js
@@ -42,7 +42,7 @@ const styles = (theme) => {
     key: {
       fontWeight: 700,
       margin: `0 ${theme.spacing.unit / 4}px`,
-      flex: '0 1 100px',
+      minWidth: 100,
     },
     value: {
       margin: -theme.spacing.unit / 4,
